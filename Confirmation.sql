e45e5f9bd88aa35d00d759040235b0f37ad6657f
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-10-15 22:27:26 +0530

    Update Confirmation.sql

--- Confirmation.sql
+++ Confirmation.sql
@@ -1,1 +1,9 @@
-
+# Write your MySQL query statement below
+SELECT sign.user_id,
+      ROUND((AVG(CASE
+                    WHEN action='confirmed' THEN 1
+                    ELSE 0
+                    END)),2) AS confirmation_rate
+FROM Signups sign
+LEFT JOIN Confirmations c ON sign.user_id=c.user_id
+GROUP BY user_id
